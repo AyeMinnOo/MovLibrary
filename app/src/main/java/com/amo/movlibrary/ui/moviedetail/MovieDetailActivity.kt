2f4d280f811be2793cbb85abf719ecb9375e251e
amo <blankcancel@gmail.com>
2018-10-26 12:37:12 +0630

    // hide and show title

--- app/src/main/java/com/amo/movlibrary/ui/moviedetail/MovieDetailActivity.kt
+++ app/src/main/java/com/amo/movlibrary/ui/moviedetail/MovieDetailActivity.kt
@@ -18,6 +18,7 @@ import com.amo.movlibrary.ui.base.BaseActivity
 import com.amo.movlibrary.ui.movielist.MovieViewHolder
 import com.amo.movlibrary.utils.setBackDropImage
 import com.amo.movlibrary.utils.setPosterImage
+import com.google.android.material.appbar.AppBarLayout
 import kotlinx.android.synthetic.main.activity_movie_detail.*
 import kotlinx.android.synthetic.main.content_movie_detail.*
 import kotlinx.android.synthetic.main.header_movie_detail.*
@@ -59,8 +60,22 @@ class MovieDetailActivity : BaseActivity<ActivityMainBinding, MovieDetailViewMod
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         vm.navigator = this
         vm.getDetail(intent?.getLongExtra(ID_TAG, -1)!!)
+        initViews()
+    }
+
+    private fun initViews() {
         imgCover.setBackDropImage(intent?.getStringExtra(COVER_TAG) ?: "")
         imgProfile.setPosterImage(intent?.getStringExtra(LOGO_TAG) ?: "")
+
+        appbarLayout.addOnOffsetChangedListener(object : AppBarLayout.OnOffsetChangedListener {
+            override fun onOffsetChanged(layout: AppBarLayout?, offset: Int) {
+                if (Math.abs(offset) == layout?.totalScrollRange) {
+                    showTitle(true)
+                } else {
+                    showTitle(false)
+                }
+            }
+        })
     }
 
     override fun bind(response: MovieDetailResponse) {
@@ -99,6 +114,10 @@ class MovieDetailActivity : BaseActivity<ActivityMainBinding, MovieDetailViewMod
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun showTitle(flag: Boolean) {
+        supportActionBar?.setDisplayShowTitleEnabled(flag)
     }
 
     override fun onMovieClick(view: ImageView, id: Long, logo: String?, cover: String?) {
